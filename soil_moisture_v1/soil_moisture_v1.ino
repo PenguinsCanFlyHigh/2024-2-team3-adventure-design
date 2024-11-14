0b611838618cd1ff61d3b4d18520817f70598d23
PenguinsCanFlyHigh <145918465+PenguinsCanFlyHigh@users.noreply.github.com>
2024-11-14 13:51:15 +0900

    Update soil_moisture_v1.ino

--- soil_moisture_v1/soil_moisture_v1.ino
+++ soil_moisture_v1/soil_moisture_v1.ino
@@ -1,11 +1,13 @@
-#define RAIN_SENSOR_ANALOG_PIN A0 // 빗물 감지 센서의 아날로그 핀 번호
+#define RAIN_SENSOR_ANALOG_PIN A8 // 빗물 감지 센서의 아날로그 핀 번호
 #define RAIN_SENSOR_DIGITAL_PIN 37  // 빗물 감지 센서의 디지털 핀 번호
-#define LED_PIN 7                 // 감지되면 켜질 LED의 핀 번호
-#define LED_PIN2 8                 // 감지안되면 켜질 LED의 핀 번호
+#define LED_PIN 9                 // 빗물이 많이 감지되면 켜질 LED의 핀 번호
+#define LED_PIN2 8                 // 빗물이 조금 감지되면 켜질 LED의 핀 번호
+#define LED_PIN3 7                 // 빗물이 감지안되면 켜질 LED의 핀 번호
 void setup() {
   pinMode(RAIN_SENSOR_DIGITAL_PIN, INPUT); // 디지털 핀을 입력으로 설정
   pinMode(LED_PIN, OUTPUT);                // LED 핀을 출력으로 설정
     pinMode(LED_PIN2, OUTPUT);                // LED 핀을 출력으로 설정
+    pinMode(LED_PIN3, OUTPUT);                // LED 핀을 출력으로 설정
   Serial.begin(9600);                      // 시리얼 통신 시작
 }
 
@@ -16,14 +18,26 @@ void loop() {
   Serial.print("아날로그 값: ");
   Serial.println(rainSensorAnalogValue); // 아날로그 값 시리얼 모니터에 출력
 
-  if (rainSensorDigitalState == HIGH) {
-    Serial.println("빗물 감지안됨!"); // 시리얼 모니터에 메시지 출력
-    digitalWrite(LED_PIN2, HIGH);    // LED 켜기
-     digitalWrite(LED_PIN, LOW);     // LED 켜기
-  } else {
-       Serial.println("빗물 감지됨!"); // 시리얼 모니터에 메시지 출력
+  if (rainSensorAnalogValue < 400) {
+      Serial.println("빗물 많이 감지됨!"); // 시리얼 모니터에 메시지 출력
     digitalWrite(LED_PIN, HIGH);     // LED 켜기
     digitalWrite(LED_PIN2, LOW);    // LED 켜기
- }
+    digitalWrite(LED_PIN3, LOW);     // LED 켜기
+  }
+
+    else if (rainSensorAnalogValue < 800) {
+      Serial.println("빗물 조금 감지됨!"); // 시리얼 모니터에 메시지 출력
+    digitalWrite(LED_PIN2, HIGH);     // LED 켜기
+    digitalWrite(LED_PIN, LOW);    // LED 켜기
+    digitalWrite(LED_PIN3, LOW);     // LED 켜기
+    }
+
+    else if (rainSensorAnalogValue > 800) {
+    Serial.println("빗물 감지안됨!"); // 시리얼 모니터에 메시지 출력
+    digitalWrite(LED_PIN3, HIGH);    // LED 켜기
+     digitalWrite(LED_PIN, LOW);     // LED 켜기
+     digitalWrite(LED_PIN2, LOW);     // LED 켜기
+    }
+
   delay(100); // 0.1초 대기
 }
