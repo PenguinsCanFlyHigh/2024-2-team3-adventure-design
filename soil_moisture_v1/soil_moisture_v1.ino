724f12395d612bccf743812e2d3fb201d56fc397
PenguinsCanFlyHigh <145918465+PenguinsCanFlyHigh@users.noreply.github.com>
2024-11-14 13:07:22 +0900

    Create soil_moisture_v1.ino

---
+++ soil_moisture_v1/soil_moisture_v1.ino
@@ -0,0 +1,29 @@
+#define RAIN_SENSOR_ANALOG_PIN A0 // 빗물 감지 센서의 아날로그 핀 번호
+#define RAIN_SENSOR_DIGITAL_PIN 37  // 빗물 감지 센서의 디지털 핀 번호
+#define LED_PIN 7                 // 감지되면 켜질 LED의 핀 번호
+#define LED_PIN2 8                 // 감지안되면 켜질 LED의 핀 번호
+void setup() {
+  pinMode(RAIN_SENSOR_DIGITAL_PIN, INPUT); // 디지털 핀을 입력으로 설정
+  pinMode(LED_PIN, OUTPUT);                // LED 핀을 출력으로 설정
+    pinMode(LED_PIN2, OUTPUT);                // LED 핀을 출력으로 설정
+  Serial.begin(9600);                      // 시리얼 통신 시작
+}
+
+void loop() {
+  int rainSensorAnalogValue = analogRead(RAIN_SENSOR_ANALOG_PIN); // 아날로그 핀을 통해 센서값 읽기
+  int rainSensorDigitalState = digitalRead(RAIN_SENSOR_DIGITAL_PIN); // 디지털 핀을 통해 센서 상태 읽기
+
+  Serial.print("아날로그 값: ");
+  Serial.println(rainSensorAnalogValue); // 아날로그 값 시리얼 모니터에 출력
+
+  if (rainSensorDigitalState == HIGH) {
+    Serial.println("빗물 감지안됨!"); // 시리얼 모니터에 메시지 출력
+    digitalWrite(LED_PIN2, HIGH);    // LED 켜기
+     digitalWrite(LED_PIN, LOW);     // LED 켜기
+  } else {
+       Serial.println("빗물 감지됨!"); // 시리얼 모니터에 메시지 출력
+    digitalWrite(LED_PIN, HIGH);     // LED 켜기
+    digitalWrite(LED_PIN2, LOW);    // LED 켜기
+ }
+  delay(100); // 0.1초 대기
+}
